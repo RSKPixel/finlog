06e52aeed0c20071988eb14cdcd0f445dcfb11ae
RSKPixel <166299316+RSKPixel@users.noreply.github.com>
2025-06-24 12:07:15 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from './pages/Dashboard';
-import Basetemplate from './templates/Basetemplate';
-import './index.css'
-import AuthContext from './templates/AuthContext';
-import Authenticator from './components/Authenticator';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Register from './pages/Register';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Basetemplate from "./templates/Basetemplate";
+import "./index.css";
+import AuthContext from "./templates/AuthContext";
+import Authenticator from "./components/Authenticator";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Register from "./pages/Register";
 
 function App() {
-
   const [loggedIn, setLoggedIn] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("finlog_token") || null);
   const [client, setClient] = useState({
-    pan: '',
-    name: '',
-    email: '',
-    phone: '',
+    pan: "",
+    name: "",
+    email: "",
+    phone: "",
   });
-  const api = "http://127.0.0.1:8000"
+  const api = "http://127.0.0.1:8000";
   const provider = {
     api,
-    loggedIn, setLoggedIn,
-    token, setToken,
-    client, setClient,
-  }
+    loggedIn,
+    setLoggedIn,
+    token,
+    setToken,
+    client,
+    setClient,
+  };
 
   return (
     <Router>
-
       <AuthContext.Provider value={provider}>
-        <Basetemplate >
+        <Basetemplate>
           <Routes>
-            <Route element={<Authenticator />} >
-              <Route path='/' element={<Dashboard />} />
+            <Route element={<Authenticator />}>
+              <Route path="/" element={<Dashboard />} />
+
+              <Route path={"/masters/ledger"} element={<div>Ledger Master</div>} />
             </Route>
           </Routes>
         </Basetemplate>
       </AuthContext.Provider>
-    </Router >
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/templates/Basetemplate.jsx
+++ frontend/src/templates/Basetemplate.jsx
@@ -1,24 +1,58 @@
-import React, { useContext } from 'react'
-import AuthContext from './AuthContext'
+import React, { useContext } from "react";
+import AuthContext from "./AuthContext";
+import { Link } from "react-router-dom";
 
 const Basetemplate = ({ children }) => {
+  const { api, loggedIn, client } = useContext(AuthContext);
+  const menu = {
+    Masters: {
+      "Ledger Master": "/masters/ledger",
+    },
+    Transactions: {
+      "Bank & Cash": "/transactions/bankcash",
+      Investments: "/transactions/investments",
+      Assets: "/transactions/assets",
+      "Capital Gains": "/transactions/capitalgains",
+      Loans: "/transactions/loans",
+      "Opening Balance": "/transactions/openingbalance",
+    },
+  };
 
-    const { api, loggedIn, client } = useContext(AuthContext)
-
-    return (
-        <div className='flex flex-col'>
-            <div className='flex flex-row fixed w-full justify-between items-center  bg-violet-700 px-4 py-1 shadow-sm text-white'>
-                <h1 className='text-2xl font-bold'>FinLog</h1>
-                <div className='ms-auto'></div>
-                {loggedIn &&
-                    <span className='font-bold'>{client.name} ({client.pan})</span>
-                }
-            </div>
-            <div className='flex flex-col mt-15 items-center justify-center text-white'>
-                {children}
-            </div>
+  return (
+    <div className="flex flex-col h-screen w-full">
+      <div className="flex flex-row h-10 w-full justify-between items-center  bg-stone-800 px-4 py-0 shadow-sm text-white border-b border-stone-700">
+        <h1 className="text-2xl font-bold">FinLog</h1>
+        <div className="ms-auto"></div>
+        {loggedIn && (
+          <span className="font-bold">
+            {client.name} ({client.pan})
+          </span>
+        )}
+      </div>
+      <div className="flex flex-row text-white h-[calc(100vh-2.5rem)] w-full">
+        <div className="flex flex-col w-48 gap-2 h-full text-lg font-semibold border-r border-stone-700 overflow-y-auto ">
+          <ul>
+            {Object.keys(menu).map((section) => (
+              <li key={section} className="mb-4">
+                <div className="font-bold text-sm w-full bg-blue-900 px-2 py-1">{section}</div>
+                <ul>
+                  {Object.entries(menu[section]).map(([name, path]) => (
+                    <li key={name} className="bg-blue-500 text-black px-2 hover:bg-blue-300 cursor-pointer">
+                      <Link to={path} className="text-xs ">
+                        {name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
-}
 
-export default Basetemplate
+        <div className="flex flex-col w-full bg-stone-900  p-2 h-full">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Basetemplate;
