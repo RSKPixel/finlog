44600563abc58676a6eb3b50ea82fac6cafe8165
RSKPixel <166299316+RSKPixel@users.noreply.github.com>
2025-06-26 10:38:33 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,54 +7,52 @@ import AuthContext from "./templates/AuthContext";
 import Authenticator from "./components/Authenticator";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Logout from "./components/Logout";
-import Instruments from "./pages/masters/Instruments";
-import Banking from "./assets/transactions/banking/Banking";
+import Banking from "./pages/transactions/banking/Banking";
 import LedgerMaster from "./pages/masters/LedgerMaster";
 
 function App() {
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [token, setToken] = useState(localStorage.getItem("finlog_token") || null);
-    const [client, setClient] = useState({
-        pan: "",
-        name: "",
-        email: "",
-        phone: "",
-    });
-    const api = "http://127.0.0.1:8000";
-    const provider = {
-        api,
-        loggedIn,
-        setLoggedIn,
-        token,
-        setToken,
-        client,
-        setClient,
-    };
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("finlog_token") || null);
+  const [client, setClient] = useState({
+    pan: "",
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const api = "http://127.0.0.1:8000";
+  const provider = {
+    api,
+    loggedIn,
+    setLoggedIn,
+    token,
+    setToken,
+    client,
+    setClient,
+  };
 
-    return (
-        <Router>
-            <AuthContext.Provider value={provider}>
-                <Basetemplate>
-                    <Routes>
-                        <Route element={<Authenticator />}>
-                            <Route path="/" element={<Dashboard />} />
+  return (
+    <Router>
+      <AuthContext.Provider value={provider}>
+        <Basetemplate>
+          <Routes>
+            <Route element={<Authenticator />}>
+              <Route path="/" element={<Dashboard />} />
 
-                            <Route path="/masters">
-                                <Route path="ledger" element={<LedgerMaster />} />
-                            </Route>
+              <Route path="/masters">
+                <Route path="ledger" element={<LedgerMaster />} />
+              </Route>
 
-                            <Route path="/transactions">
-                                <Route path="banking" element={<Banking />} />
-                            </Route>
+              <Route path="/transactions">
+                <Route path="banking" element={<Banking />} />
+              </Route>
+            </Route>
 
-                        </Route>
-
-                        <Route path="/logout" element={<Logout />} />
-                    </Routes>
-                </Basetemplate>
-            </AuthContext.Provider>
-        </Router>
-    );
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </Basetemplate>
+      </AuthContext.Provider>
+    </Router>
+  );
 }
 
 export default App;
