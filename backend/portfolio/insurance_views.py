a04ab0fa69d48b354b40d570a65e9537e3f973b8
RSKPixel <166299316+RSKPixel@users.noreply.github.com>
2025-07-03 13:27:46 +0530

    update

--- backend/portfolio/insurance_views.py
+++ backend/portfolio/insurance_views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from portfolio.models import Insurance, InsuranceTransactions
+from django.db.models import Sum
+import pandas as pd
 
 
 @api_view(['POST'])
@@ -8,21 +10,38 @@ def insurance(request):
     client_pan = request.data.get('client_pan')
 
     if not client_pan:
-        return Response({"status": "error","message": "Client PAN is required", "data": []})
+        return Response({"status": "error", "message": "Client PAN is required", "data": []})
 
     insurances = Insurance.objects.filter(client_pan=client_pan).values()
 
+    for insurance in insurances:
+        total_premium_paid = InsuranceTransactions.objects.filter(
+            client_pan=client_pan,
+            policy_no=insurance['policy_no'],
+            transaction_type='premium'
+        ).aggregate(total_paid=Sum('transaction_amount'))['total_paid']
+
+        Insurance.objects.filter(
+            client_pan=client_pan,
+            policy_no=insurance['policy_no']
+        ).update(total_premium_paid=total_premium_paid)
+
+    insurances = Insurance.objects.filter(client_pan=client_pan).values()
+    df = pd.DataFrame(list(insurances))
+    df.fillna(0, inplace=True)
+
     if not insurances:
-        return Response({"status":"error", "message": "No insurance data found for the given PAN", "data": []})
-    
-    return Response({"status":"success","message": "Data fetched", "data": list(insurances)})
+        return Response({"status": "error", "message": "No insurance data found for the given PAN", "data": []})
+
+    return Response({"status": "success", "message": "Data fetched", "data": df.to_dict(orient='records')})
+
 
 @api_view(['POST'])
 def insurance_save(request):
 
     action = request.data.get('action')
     client_pan = request.data.get('client_pan')
-    policy_name = str.upper(request.data.get('policy_name')) 
+    policy_name = str.upper(request.data.get('policy_name'))
     policy_no = request.data.get('policy_no')
     policy_type = request.data.get('policy_type')
     insurer = str.upper(request.data.get('insurer'))
@@ -38,7 +57,8 @@ def insurance_save(request):
     current_value = request.data.get('current_value')
     remarks = request.data.get('remarks')
 
-    print(client_pan, policy_name, policy_no, policy_type, insurer, date_of_commencement, date_of_last_premium, date_of_maturity, premium_amount, sum_assured, frequency, agent_name, policy_status, remarks)
+    print(client_pan, policy_name, policy_no, policy_type, insurer, date_of_commencement, date_of_last_premium,
+          date_of_maturity, premium_amount, sum_assured, frequency, agent_name, policy_status, remarks)
 
     if not client_pan or not policy_name or not policy_no:
         return Response({"status": "error", "message": "Client PAN, Policy Name and Policy Number are required"})
@@ -63,10 +83,11 @@ def insurance_save(request):
             remarks=remarks
         )
         insurance.save()
-        return Response({"status": "success", "message": "Insurance policy added successfully", "data": [] })
+        return Response({"status": "success", "message": "Insurance policy added successfully", "data": []})
     elif action == 'modify':
         try:
-            insurance = Insurance.objects.get(client_pan=client_pan, policy_no=policy_no)
+            insurance = Insurance.objects.get(
+                client_pan=client_pan, policy_no=policy_no)
             insurance.policy_name = policy_name
             insurance.policy_type = policy_type
             insurance.insurer = insurer
@@ -83,18 +104,20 @@ def insurance_save(request):
             insurance.remarks = remarks
 
             insurance.save()
-            return Response({"status": "success", "message": "Insurance policy updated successfully", "data": [] })
+            return Response({"status": "success", "message": "Insurance policy updated successfully", "data": []})
         except Insurance.DoesNotExist:
-            return Response({"status": "error", "message": "Insurance policy not found", "data": [] })
+            return Response({"status": "error", "message": "Insurance policy not found", "data": []})
     elif action == 'delete':
         try:
-            insurance = Insurance.objects.get(client_pan=client_pan, policy_no=policy_no)
+            insurance = Insurance.objects.get(
+                client_pan=client_pan, policy_no=policy_no)
             insurance.delete()
-            return Response({"status": "success", "message": "Insurance policy deleted successfully", "data": [] })
+            return Response({"status": "success", "message": "Insurance policy deleted successfully", "data": []})
         except Insurance.DoesNotExist:
-            return Response({"status": "error", "message": "Insurance policy not found", "data": [] })
+            return Response({"status": "error", "message": "Insurance policy not found", "data": []})
 
-    return Response({"status": "error", "message": "Invalid action", "data": [] })
+    return Response({"status": "error", "message": "Invalid action", "data": []})
+
 
 @api_view(['POST'])
 def insurance_transactions(request):
@@ -104,12 +127,14 @@ def insurance_transactions(request):
     if not client_pan or not policy_no:
         return Response({"status": "error", "message": "Client PAN and Policy Number are required", "data": []})
 
-    transactions = InsuranceTransactions.objects.filter(client_pan=client_pan, policy_no=policy_no).values()
+    transactions = InsuranceTransactions.objects.filter(
+        client_pan=client_pan, policy_no=policy_no).values()
 
     if not transactions:
         return Response({"status": "error", "message": "No insurance transactions found for the given PAN and Policy Number", "data": []})
 
     return Response({"status": "success", "message": "Data fetched", "data": list(transactions)})
+
 
 @api_view(['POST'])
 def insurance_transactions_save(request):
@@ -133,23 +158,25 @@ def insurance_transactions_save(request):
             transaction_amount=transaction_amount,
         )
         insurance_transaction.save()
-        return Response({"status": "success", "message": "Insurance transaction added successfully", "data": [] })
+        return Response({"status": "success", "message": "Insurance transaction added successfully", "data": []})
     elif action == 'modify':
         try:
-            insurance_transaction = InsuranceTransactions.objects.get(id=transaction_id)
+            insurance_transaction = InsuranceTransactions.objects.get(
+                id=transaction_id)
             insurance_transaction.transaction_type = transaction_type
             insurance_transaction.transaction_amount = transaction_amount
 
             insurance_transaction.save()
-            return Response({"status": "success", "message": "Insurance transaction updated successfully", "data": [] })
+            return Response({"status": "success", "message": "Insurance transaction updated successfully", "data": []})
         except InsuranceTransactions.DoesNotExist:
-            return Response({"status": "error", "message": "Insurance transaction not found", "data": [] })
+            return Response({"status": "error", "message": "Insurance transaction not found", "data": []})
     elif action == 'delete':
         try:
-            insurance_transaction = InsuranceTransactions.objects.get(client_pan=client_pan, policy_no=policy_no, transaction_date=transaction_date)
+            insurance_transaction = InsuranceTransactions.objects.get(
+                client_pan=client_pan, policy_no=policy_no, transaction_date=transaction_date)
             insurance_transaction.delete()
-            return Response({"status": "success", "message": "Insurance transaction deleted successfully", "data": [] })
+            return Response({"status": "success", "message": "Insurance transaction deleted successfully", "data": []})
         except InsuranceTransactions.DoesNotExist:
-            return Response({"status": "error", "message": "Insurance transaction not found", "data": [] })
-    
-    return Response({"status": "success", "message": "Insurance transaction saved successfully", "data": [] })
+            return Response({"status": "error", "message": "Insurance transaction not found", "data": []})
+
+    return Response({"status": "success", "message": "Insurance transaction saved successfully", "data": []})
--- frontend/src/pages/portfolio/insurance/Insurance.jsx
+++ frontend/src/pages/portfolio/insurance/Insurance.jsx
@@ -1,98 +1,145 @@
-import React, { useContext, useEffect, useState } from 'react'
-import AuthContext from '../../../templates/AuthContext'
-import numeral from 'numeral'
-import PolicyForm from './PolicyForm'
-import PolicyTransactions from './PolicyTransactions'
+import React, { useContext, useEffect, useState } from "react";
+import AuthContext from "../../../templates/AuthContext";
+import numeral from "numeral";
+import PolicyForm from "./PolicyForm";
+import PolicyTransactions from "./PolicyTransactions";
+import moment from "moment";
 
 const Insurance = () => {
-    const { api, token, client } = useContext(AuthContext)
-    const [showForm, setShowForm] = useState(false)
-    const [addTransaction, setAddTransaction] = useState(false)
-    const [insuranceData, setInsuranceData] = useState([])
-    const [modifyItem, setModifyItem] = useState({})
+  const { api, token, client } = useContext(AuthContext);
+  const [showForm, setShowForm] = useState(false);
+  const [addTransaction, setAddTransaction] = useState(false);
+  const [insuranceData, setInsuranceData] = useState([]);
+  const [modifyItem, setModifyItem] = useState({});
 
+  useEffect(() => {
+    fetch(`${api}/portfolio/insurance/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      body: JSON.stringify({
+        client_pan: client.pan,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setInsuranceData(data.data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(`${api}/portfolio/insurance/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Token ${token}`,
-            },
-            body: JSON.stringify({
-                client_pan: client.pan,
-            })
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setInsuranceData(data.data)
-            })
-    }, [])
+  const handleModify = (item) => {
+    setShowForm(true);
+    setModifyItem({
+      ...item,
+    });
+  };
 
-    const handleModify = (item) => {
-        setShowForm(true)
-        setModifyItem({
-            ...item
-        })
-    }
+  const handleAddTransaction = (item) => {
+    setAddTransaction(true);
+    setModifyItem({ ...item });
+  };
+  return (
+    <div className="flex flex-col gap-0 w-full items-center">
+      <div className="flex flex-col w-full items-center">
+        <div className="w-full px-2 text-sm font-bold z-10 border border-sky-900 py-1 rounded-t-sm bg-sky-950 flex flex-row items-center">
+          <span>Mutual Fund Holdings</span>
+          <span className="ms-auto"></span>
+          <span
+            className="cursor-pointer"
+            onClick={() => {
+              setModifyItem([]);
+              setShowForm(true);
+            }}
+          >
+            <i className="bi bi-plus-circle hover:text-yellow-400 font-bold"></i>
+          </span>
+        </div>
+      </div>
 
-    const handleAddTransaction = (item) => {
-        setAddTransaction(true)
-        setModifyItem({ ...item })
-    }
-    return (
-        <div className="flex flex-col gap-0 w-full items-center">
-            <div className="flex flex-col w-full items-center">
-                <div className="w-full px-2 text-sm font-bold z-10 border border-sky-900 py-1 rounded-t-sm bg-sky-950 flex flex-row items-center">
-                    <span>Mutual Fund Holdings</span>
-                    <span className="ms-auto"></span>
-                    <span className='cursor-pointer' onClick={() => { setModifyItem([]); setShowForm(true) }}><i className='bi bi-plus-circle hover:text-yellow-400 font-bold'></i></span>
-                </div>
-            </div>
+      {/* <div className="grid grid-cols-[2fr_1fr_2fr_1fr_0.75fr_0.75fr_1fr_0.5fr] gap-2 w-full py-2 px-2 border-t bg-stone-900 border border-sky-900">
+        <div className="font-bold text-sm">Policy Name</div>
+        <div className="font-bold text-sm">Policy No</div>
+        <div className="font-bold text-sm">Insurer</div>
+        <div className="font-bold text-sm text-center">Policy Type</div>
+        <div className="font-bold text-sm text-end">Premium</div>
+        <div className="font-bold text-sm text-end">Sum Assured</div>
+        <div className="font-bold text-sm text-center">Status</div>
+        <div className="font-bold text-sm text-center">Action</div>
+      </div> */}
+      {insuranceData.map((item, index) => (
+        <div key={index} className="grid grid-cols-[1fr_1fr_1fr_1fr] justify-center gap-1 w-full p-2 bg-stone-800  border-t-0 border border-sky-900 hover:bg-stone-700 cursor-default">
+          {/* row 1 */}
 
-            <div className="grid grid-cols-[2fr_1fr_2fr_1fr_0.75fr_0.75fr_1fr_0.5fr] gap-2 w-full py-2 px-2 border-t bg-stone-900 border border-sky-900">
-                <div className="font-bold text-sm">Policy Name</div>
-                <div className="font-bold text-sm">Policy No</div>
-                <div className="font-bold text-sm">Insurer</div>
-                <div className="font-bold text-sm text-center">Policy Type</div>
-                <div className="font-bold text-sm text-end">Premium</div>
-                <div className="font-bold text-sm text-end">Sum Assured</div>
-                <div className="font-bold text-sm text-center">Status</div>
-                <div className="font-bold text-sm text-center">Action</div>
-            </div>
-            {
-                insuranceData.map((item, index) => (
-                    <div key={index} className="grid grid-cols-[2fr_1fr_2fr_1fr_0.75fr_0.75fr_1fr_0.5fr] justify-center gap-2 w-full p-2 bg-stone-800  border-t-0 border border-sky-900 hover:bg-stone-700 cursor-default">
-                        <div className="text-sm text-nowrap overflow-hidden text-ellipsis">{item.policy_name}</div>
-                        <div className="text-sm">{item.policy_no}</div>
-                        <div className="text-sm text-nowrap overflow-hidden text-ellipsis">{item.insurer}</div>
-                        <div className="text-sm text-center">{item.policy_type}</div>
-                        <div className="text-sm text-end">{numeral(item.premium_amount).format("0,0.00")}</div>
-                        <div className="text-sm text-end">{numeral(item.sum_assured).format("0,0.00")}</div>
-                        <div className="text-sm text-center">{item.policy_status}</div>
-                        <div className='flex flex-row gap-2 justify-center items-center'>
+          {/* Row 2 */}
+          {/* 1 */}
+          <div>
+            <label>Policy Name</label>
+            <div className="text-sm text-yellow-100 text-nowrap overflow-hidden text-ellipsis">{item.policy_name}</div>
+          </div>
+          <div>
+            <label>Frequency</label>
+            <div className="text-sm text-yellow-100">{item.frequency}</div>
+          </div>
+          <div>
+            <label>Date of Maturity</label>
+            <div className="text-sm text-yellow-100">{moment(item.date_of_maturity).format("DD-MMMM-YYYY")}</div>
+          </div>
+          <div>
+            <label>Premium Amount</label>
+            <div className="text-sm text-yellow-100">{numeral(item.premium_amount).format("0,0.00")}</div>
+          </div>
 
-                            <span className='text-center' onClick={() => handleModify(item)}>
-                                <i className="bi bi-pencil-square  cursor-pointer hover:text-yellow-400"></i>
-                            </span>
-                            <span className='text-center'>
-                                <i className="bi bi-plus-circle  cursor-pointer hover:text-yellow-400" onClick={() => handleAddTransaction(item)}></i>
-                            </span>
-                        </div>
-                    </div>
-                ))
-            }
-            {showForm &&
-                <PolicyForm showForm={showForm} setShowForm={setShowForm} modifyItem={modifyItem} setInsuranceData={setInsuranceData} />
-            }
+          <div>
+            <label>Insurer</label>
+            <div className="text-sm text-yellow-100 text-nowrap overflow-hidden text-ellipsis">{item.insurer}</div>
+          </div>
+          <div>
+            <label>Policy Status</label>
+            <div className="text-sm text-yellow-100">{item.policy_status}</div>
+          </div>
+          <div>
+            <label>Date of Commencement</label>
+            <div className="text-sm text-yellow-100">{moment(item.date_of_commencement).format("DD-MMMM-YYYY")}</div>
+          </div>
+          <div>
+            <label>Total Premium Paid</label>
+            <div className="text-sm text-yellow-100">{numeral(item.total_premium_paid).format("0,0.00")}</div>
+          </div>
 
-            {addTransaction &&
-                <PolicyTransactions addTransaction={addTransaction} setAddTransaction={setAddTransaction} insuranceData={modifyItem} />
-            }
+          <div>
+            <label>Policy No</label>
+            <div className="text-sm text-yellow-100">{item.policy_no}</div>
+          </div>
+          <div>
+            <label>Agent Name</label>
+            <div className="text-sm text-yellow-100 text-nowrap overflow-hidden text-ellipsis">{item.agent_name}</div>
+          </div>
+          <div>
+            <label>Sum Assured</label>
+            <div className="text-sm text-yellow-100">{numeral(item.sum_assured).format("0,0.00")}</div>
+          </div>
+          <div>
+            <label>Current Value</label>
+            <div className="text-sm text-yellow-100">{numeral(item.current_value).format("0,0.00")}</div>
+          </div>
 
-        </div >
-    )
-}
+          {/* <div className="flex flex-row gap-2 justify-center items-center">
+            <span className="text-center" onClick={() => handleModify(item)}>
+              <i className="bi bi-pencil-square  cursor-pointer hover:text-yellow-400"></i>
+            </span>
+            <span className="text-center">
+              <i className="bi bi-plus-circle  cursor-pointer hover:text-yellow-400" onClick={() => handleAddTransaction(item)}></i>
+            </span>
+          </div> */}
+        </div>
+      ))}
+      {showForm && <PolicyForm showForm={showForm} setShowForm={setShowForm} modifyItem={modifyItem} setInsuranceData={setInsuranceData} />}
 
+      {addTransaction && <PolicyTransactions addTransaction={addTransaction} setAddTransaction={setAddTransaction} insuranceData={modifyItem} />}
+    </div>
+  );
+};
 
-export default Insurance
+export default Insurance;
